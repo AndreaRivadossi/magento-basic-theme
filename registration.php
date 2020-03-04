850ab55dc52d4046baebd823d4cc7a2222530697
Andrea Rivadossi <riva.andrea.web@gmail.com>
2020-03-04 12:18:04 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Copyright © 2015 Magento. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/<your_vendor_name>/<your_theme_name>',
-    __DIR__
-);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/<your_vendor_name>/<your_theme_name>', __DIR__);
